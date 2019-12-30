52ec696e85cb1f0485fd477435504de84878979a
Manjaka97 <manjaka.andriamasinoro@gmail.com>
2019-12-30 14:50:20 -0500

    Implemented delete_party and added check for party name unicity

--- main.py
+++ main.py
@@ -28,11 +28,36 @@ class Main(QtWidgets.QMainWindow, window.UiMainWindow):
             self.contract_window = ContractWindow(self.contract_name, self.project_name)
             self.contract_window.setWindowModality(QtCore.Qt.ApplicationModal)
             self.contract_window.contract_tree_signal.connect(self.update_contract_tree)
-            self.contract_window.party_signal.connect(self.new_party_window)
+            self.contract_window.new_party_signal.connect(self.new_party_window)
+            self.contract_window.edit_party_signal.connect(self.edit_party_window)
+            self.contract_window.delete_party_signal.connect(self.delete_party)
             self.contract_window.show()
+
+    def run_query(self, query, values=()):
+        sqliteConnection = sqlite3.connect('Contracts.db')
+        cursor = sqliteConnection.cursor()
+        print('Connected to SQLite')
+        cursor.execute(query, values)
+        sqliteConnection.commit()
+        print('Query executed')
+        cursor.close()
+
+    def delete_party(self, name, project_id):
+        prompt = 'Are you sure you want to delete ' + name + '?'
+        buttonReply = QMessageBox.question(self, 'Delete Party', prompt,
+                                           QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if buttonReply == QMessageBox.Yes:
+            self.run_query('DELETE FROM parties WHERE name=? AND project_id=?', (name, project_id))
+            self.contract_window.update_parties_tree()
 
     def new_party_window(self):
         self.party_window = PartyWindow(self.contract_window.project_name)
+        self.party_window.setWindowModality(QtCore.Qt.ApplicationModal)
+        self.party_window.party_changed.connect(self.update_parties_tree)
+        self.party_window.show()
+
+    def edit_party_window(self, name, first, last, phone, email):
+        self.party_window = PartyWindow(self.contract_window.project_name, name=name, first=first, last=last, phone=phone, email=email, update=True)
         self.party_window.setWindowModality(QtCore.Qt.ApplicationModal)
         self.party_window.party_changed.connect(self.update_parties_tree)
         self.party_window.show()
@@ -109,7 +134,9 @@ class Main(QtWidgets.QMainWindow, window.UiMainWindow):
 
 class ContractWindow(QtWidgets.QWidget, contract.Ui_Form):
     contract_tree_signal = QtCore.pyqtSignal()
-    party_signal = QtCore.pyqtSignal()
+    new_party_signal = QtCore.pyqtSignal()
+    edit_party_signal = QtCore.pyqtSignal(str, str, str, str, str)
+    delete_party_signal = QtCore.pyqtSignal(str, int)
 
     def __init__(self, contract_name, project_name):
         QtWidgets.QWidget.__init__(self)
@@ -154,6 +181,8 @@ class ContractWindow(QtWidgets.QWidget, contract.Ui_Form):
         self.contract_ui.pushButton_12.clicked.connect(self.edit_contract)
         self.contract_ui.pushButton_13.clicked.connect(self.edit_project)
         self.contract_ui.pushButton.clicked.connect(self.add_party)
+        self.contract_ui.pushButton_2.clicked.connect(self.edit_party)
+        self.contract_ui.pushButton_3.clicked.connect(self.delete_party)
 
     # Helping method to run simple queries
     def run_query(self, query, values=()):
@@ -262,15 +291,31 @@ class ContractWindow(QtWidgets.QWidget, contract.Ui_Form):
         self.contract_tree_signal.emit()
 
     def add_party(self):
-        self.party_signal.emit()
+        self.new_party_signal.emit()
 
     def edit_party(self):
-        # TODO: Implement edit_party
-        pass
+        if self.contract_ui.parties_tree.selectedIndexes() == []:
+            return
+        else:
+            name_index = self.contract_ui.parties_tree.selectedIndexes()[0]
+            name = name_index.model().itemData(name_index)[0]
+            first_index = self.contract_ui.parties_tree.selectedIndexes()[1]
+            first = first_index.model().itemData(first_index)[0]
+            last_index = self.contract_ui.parties_tree.selectedIndexes()[2]
+            last = last_index.model().itemData(last_index)[0]
+            phone_index = self.contract_ui.parties_tree.selectedIndexes()[3]
+            phone = phone_index.model().itemData(phone_index)[0]
+            email_index = self.contract_ui.parties_tree.selectedIndexes()[4]
+            email = email_index.model().itemData(email_index)[0]
+            self.edit_party_signal.emit(str(name), str(first), str(last), str(phone), str(email))
 
     def delete_party(self):
-        # TODO: Implement delete_party
-        pass
+        if self.contract_ui.parties_tree.selectedIndexes() == []:
+            return
+        else:
+            name_index = self.contract_ui.parties_tree.selectedIndexes()[0]
+            name = name_index.model().itemData(name_index)[0]
+            self.delete_party_signal.emit(str(name), self.project_id)
 
     def add_task(self):
         # TODO: Implement add_task
@@ -298,32 +343,47 @@ class ContractWindow(QtWidgets.QWidget, contract.Ui_Form):
 class PartyWindow(QtWidgets.QWidget, party.Ui_partyDialog):
     party_changed = QtCore.pyqtSignal()
 
-    def __init__(self, project_name):
+    def __init__(self, project_name, name='', first='', last='', phone='', email='', update=False):
         QtWidgets.QWidget.__init__(self)
         self.project_name = project_name
+        self.name = name
+        self.first = first
+        self.last = last
+        self.phone = phone
+        self.email = email
+        self.update = update
 
         # Project ID:
         db = QtSql.QSqlDatabase.addDatabase("QSQLITE")
         db.setDatabaseName('Contracts.db')
         if not db.open():
             print('Db not open')
-        self.notes_model = QtSql.QSqlRelationalTableModel()
-        self.notes_model.setTable('projects')
         query = QtSql.QSqlQuery()
         query.prepare("SELECT id FROM projects where name=?")
         query.addBindValue(self.project_name)
         query.exec_()
-
         if query.next():
             self.project_id = query.value(0)
+        db.close()
 
+        # Party ID:
+        db = QtSql.QSqlDatabase.addDatabase("QSQLITE")
+        db.setDatabaseName('Contracts.db')
+        if not db.open():
+            print('Db not open')
+        query = QtSql.QSqlQuery()
+        query.prepare("SELECT id FROM parties where name=?")
+        query.addBindValue(self.name)
+        query.exec_()
+        if query.next():
+            self.party_id = query.value(0)
         db.close()
 
         self.party_ui = party.Ui_partyDialog()
-        self.party_ui.setupUi(self, self.project_id)
+        self.party_ui.setupUi(self, self.project_id, self.name, self.first, self.last, self.phone, self.email)
 
         self.party_ui.pushButton.clicked.connect(self.save_party)
-        self.party_ui.pushButton.clicked.connect(QtWidgets.QApplication.closeAllWindows)
+        self.party_changed.connect(self.close)
         self.party_ui.pushButton_2.clicked.connect(self.close)
 
     def run_query(self, query, values=()):
@@ -354,10 +414,38 @@ class PartyWindow(QtWidgets.QWidget, party.Ui_partyDialog):
         phone = self.party_ui.lineEdit_4.text()
         email = self.party_ui.lineEdit_5.text()
 
-        if name != '' and first != '' and last != '' and phone != '' and email != '':
-            self.run_query('INSERT INTO parties (project_id, name, representative_first, representative_last, phone, '
-                           'email) VALUES (?, ?, ?, ?, ?, ?)', (self.project_id, name, first, last, phone, email))
-            self.party_changed.emit()
+        existing_parties = self.fetch_query('SELECT name FROM parties WHERE project_id=?', (self.project_id,))
+
+        if name == '':
+            buttonReply = QMessageBox.question(self, 'Empty Name','Name cannot be blank',
+                                               QMessageBox.Ok)
+        else:
+            if self.update is True:
+                if name == self.name:
+                    self.run_query(
+                        'Update parties SET name=?, representative_first=?, representative_last=?, phone=?, email=? WHERE '
+                        'project_id=? AND id=?', (name, first, last, phone, email, self.project_id, self.party_id))
+                    self.party_changed.emit()
+
+                elif name in existing_parties:
+                    prompt = name + ' already exists. Please choose another name'
+                    buttonReply = QMessageBox.question(self, 'Name taken', prompt, QMessageBox.Ok)
+
+                else:
+                    self.run_query(
+                        'Update parties SET name=?, representative_first=?, representative_last=?, phone=?, email=? WHERE '
+                        'project_id=? AND id=?', (name, first, last, phone, email, self.project_id, self.party_id))
+                    self.party_changed.emit()
+            else:
+                if name in existing_parties:
+                    prompt = name + ' already exists. Please choose another name'
+                    buttonReply = QMessageBox.question(self, 'Name taken', prompt, QMessageBox.Ok)
+
+                else:
+                    self.run_query(
+                        'INSERT INTO parties (project_id, name, representative_first, representative_last, phone, '
+                        'email) VALUES (?, ?, ?, ?, ?, ?)', (self.project_id, name, first, last, phone, email))
+                    self.party_changed.emit()
 
 if __name__ == "__main__":
     import sys
